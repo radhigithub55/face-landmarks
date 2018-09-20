6c29fa0b4a62dc83b1cbd2a7d3575f59bcaeb727
CH0007 <ch0007@CH0007s-MacBook-Pro.local>
2018-09-20 18:59:46 +0530

    Add Project

---
+++ face-landmarks/face-landmarks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  face-landmarks
+//
+//  Created by CH0007 on 20/09/18.
+//  Copyright © 2018 CH0007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ face-landmarks/face-landmarksTests/face_landmarksTests.swift
@@ -0,0 +1,34 @@
+//
+//  face_landmarksTests.swift
+//  face-landmarksTests
+//
+//  Created by CH0007 on 20/09/18.
+//  Copyright © 2018 CH0007. All rights reserved.
+//
+
+import XCTest
+@testable import face_landmarks
+
+class face_landmarksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
